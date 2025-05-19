94186c8811e50105f6313ab51e4cb39628b4d8c2
kayqueR69 <kayquericharlyson@gmail.com>
2025-05-19 18:20:42 -0300

    add: primeiras alterações no projeto

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home } from "./src/pages/Home";
+import { Favorites } from "./src/pages/Favorites";
+const Tab  = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                
+                <Tab.Screen name="home" component={Home}/>
+                <Tab.Screen name="favorites" component={Favorites}/>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+            </Tab.Navigator>    
+        </NavigationContainer>
+    );
+}
---
+++ src/pages/Favorites.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export function Favorites () {
+    return (    
+        <View>
+            <Text>Favoritos</Text>
+        </View>
+    )
+}
